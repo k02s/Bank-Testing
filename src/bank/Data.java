773d762db90f5a954bc6847f7b70d28363ed28a9
k02s <khsaleh022@gmail.com>
2023-12-31 02:46:08 +0300

    'update'

--- src/bank/Data.java
+++ src/bank/Data.java
@@ -1,11 +1,26 @@
 package bank;
 
+import java.util.Random;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Data {
 	
 	WebDriver driver = new ChromeDriver();
+	Random rand = new Random();
 	
-	String BankURL = "https://globalsqa.com/angularJs-protractor/BankingProject/#/login";
+	static String BankURL = "https://globalsqa.com/angularJs-protractor/BankingProject/#/login";
+	
+	static String FirstNames[] = { "Khadijah", "Sabah", "Eman" };
+	static String LastNames[] = { "Ahmad", "Saleh", "Ali" };
+	int RandomNumberForNameIndex = rand.nextInt(3);
+
+	String first_Name = FirstNames[RandomNumberForNameIndex];
+	String last_Name = LastNames[RandomNumberForNameIndex];
+	
+	static String PostCode[] = {"123", "345" , "567"};
+	int randomPostCode = rand.nextInt(3);
+	String post_Code = PostCode[randomPostCode];
+	
 }
--- src/bank/TestCases.java
+++ src/bank/TestCases.java
@@ -1,5 +1,12 @@
 package bank;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -10,16 +17,79 @@ public class TestCases extends Data {
 	public void setup() {
 		driver.get(BankURL);
 		driver.manage().window().maximize();
-		
+
+	}
+	@Test(priority = 1)
+	public void go() {
+//		1- go this website and press one bank manager login
+			WebElement Bank_Manager_Login_Button = driver.findElement(By.cssSelector("button[ng-click='manager()']"));
+			Bank_Manager_Login_Button.click();
 	}
 	
-	@Test()
-	public void firstTest() {
-		
+	@Test(priority = 2) 
+	public void addCustomer() throws InterruptedException {
+//		2- add one customer ( random details)
+
+			Thread.sleep(5000);
+			WebElement Add_Customer_Button = driver.findElement(By.cssSelector("button[ng-click='addCust()']"));
+			Add_Customer_Button.click();
+
+			Thread.sleep(5000);
+			WebElement firstName = driver.findElement(By.cssSelector("input[placeholder='First Name']"));
+			WebElement lastName = driver.findElement(By.cssSelector("input[placeholder='Last Name']"));
+			WebElement postCode = driver.findElement(By.cssSelector("input[placeholder='Post Code']"));
+			WebElement addCustomerBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+
+			
+			firstName.sendKeys(first_Name);
+			lastName.sendKeys(last_Name);
+			postCode.sendKeys(post_Code);
+			
+			Thread.sleep(5000);
+			addCustomerBtn.click();
+
+			driver.switchTo().alert().accept();
 	}
 	
+	@Test(priority = 3)
+	public void firstTest() throws InterruptedException {
+		Thread.sleep(5000);
+
+
+
+//	3- and open account for that customer that you added (accept the alert msg )
+		WebElement openAccoutBtn = driver.findElement(By.cssSelector("button[ng-click='openAccount()']"));
+		openAccoutBtn.click();
+
+		Thread.sleep(5000);
+		WebElement customer = driver.findElement(By.id("userSelect"));
+		Select select = new Select(customer);
+		select.selectByIndex(1);
+
+//	4- select the currency to be randomly selected	
+
+		List<WebElement> currencyList = driver.findElements(By.id("currency"));
+		Select currencyDropdown = new Select(currencyList.get(0)); // Assuming there is only one dropdown
+		List<WebElement> currencyOptions = currencyDropdown.getOptions();
+		int randomCurrencyIndex = rand.nextInt(currencyOptions.size());
+		currencyDropdown.selectByIndex(randomCurrencyIndex);
+		
+//	5- go to the customers and delete that customer create one assertion that the customer is deleted
+
+		WebElement customerBtn = driver.findElement(By.cssSelector("button[ng-click='showCust()']"));
+		customerBtn.click();
+		
+		Thread.sleep(5000);
+		WebElement deleteBtn = driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/table/tbody/tr[6]/td[5]/button"));
+		deleteBtn.click();
+		Assert.assertTrue(true);
+		
+//	6- add the customer again
+		addCustomer();
+	}
+
 	@AfterTest()
 	public void afterTest() {
-		
+
 	}
 }
